2a5805b06f4c3fae72e1cfff7400ed8bc1f8258f
heznpc <heznpc@gmail.com>
2025-09-22 03:55:24 +0900

    [refactor] user/src/pages 추가중 (1)

--- apps/user/src/App.tsx
+++ apps/user/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import {Route} from "lucide-react";
+import ErrorPage from "./pages/error/ErrorPage.tsx";
 
 function App() {
 
   return (
-    <></>
+      <Route>
+          <ErrorPage />
+      </Route>
   )
 }
 
